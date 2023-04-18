d4e9f321420d7ff880cde1bac9415490172f2182
matto <1175526477@qq.com>
2023-04-18 22:38:03 +0800

    fix: fix deploy bug

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import Link from '@/components/link.vue';
-
-defineProps<{ msg: string }>();
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve('src'),
+      '@': path.join(__dirname, 'src'),
     },
   },
 });
