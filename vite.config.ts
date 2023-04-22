82171d4ab2e79a166eddbb4c2644e863fbac70b4
Lee <1769712655@qq.com>
2023-04-22 20:38:53 +0800

    fix: monaco worker warning

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import wasm from "vite-plugin-wasm";
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 import topLevelAwait from "vite-plugin-top-level-await";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(),topLevelAwait()],
+  plugins: [vue(), topLevelAwait(), wasm(), (monacoEditorPlugin as any).default({})],
   css: {
     preprocessorOptions: {
       less: {
