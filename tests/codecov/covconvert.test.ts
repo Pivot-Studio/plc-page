12382cc0ec0daff670432a48b7223fd8f49690bb
bobxli <bobxli@tencent.com>
2023-04-27 16:46:26 +0800

    test: add sunburst data test

--- tests/codecov/covconvert.test.ts
+++ tests/codecov/covconvert.test.ts
@@ -1,12 +1,31 @@
 // import { useDetectDeviceSize } from '../../src';
 
-import { getSunBurstData, getTotalCov } from "../../src/hooks/codecov";
+import { expect } from "chai";
+import {
+  SunBurstData,
+  getSunBurstData,
+  getTotalCov,
+} from "../../src/hooks/codecov";
 
-
-describe('cov sunburst', function() {
-    it('sunburst convert', async function() {
-        const data = await getTotalCov();
-        const sunburst = getSunBurstData(data);
-        console.log(sunburst);
-    }); 
+describe("cov sunburst", function () {
+  it("sunburst convert", async function () {
+    const data = await getTotalCov();
+    const sunburst = getSunBurstData(data);
+    testSubNodes(sunburst);
   });
+});
+
+function testSubNodes(node: SunBurstData) {
+  expect(node.coverage, node.filename).to.eq(node.hits / node.value);
+  let totalHits = 0;
+  let totalValue = 0;
+  if (node.children && node.children.length > 0) {
+    node.children.forEach((child) => {
+      testSubNodes(child);
+      totalHits += child.hits;
+      totalValue += child.value;
+    });
+    expect(node.hits, node.filename).to.eq(totalHits);
+    expect(node.value, node.filename).to.eq(totalValue);
+  }
+}
