faf6979b4cf2e7c20d14f84ebb56664b9c9b9094
matto <1175526477@qq.com>
2023-05-13 19:11:32 +0800

    feat:add more info mobile css

--- src/components/moreInfo.vue
+++ src/components/moreInfo.vue
@@ -6,30 +6,40 @@ import { Link } from '@/components';
     <div class="container">
       <div class="line">
         <div class="title">Why PL</div>
-        <Link href="https://lang.pivotstudio.cn/docs/systemlib/immix.html"><div>immix GC</div></Link>
-        <Link href="https://lang.pivotstudio.cn/docs/tutorial/vscsupport.html"><div>VSC Support</div></Link>
-        <Link href="https://lang.pivotstudio.cn/docs/references/module.html"><div>Module</div></Link>
+        <div class="content">
+          <Link href="https://lang.pivotstudio.cn/docs/systemlib/immix.html"><div>immix GC</div></Link>
+          <Link href="https://lang.pivotstudio.cn/docs/tutorial/vscsupport.html"><div>VSC Support</div></Link>
+          <Link href="https://lang.pivotstudio.cn/docs/references/module.html"><div>Module</div></Link>
+        </div>
       </div>
       <div class="line">
         <div class="title">Get Started</div>
-        <Link href="https://lang.pivotstudio.cn/docs/index.html"><div>Documentation</div></Link>
-        <Link href="https://lang.pivotstudio.cn/docs/tutorial/index.html"><div>Tutorial</div></Link>
-        <Link href="https://lang.pivotstudio.cn/docs/references/index.html"><div>API Reference</div></Link>
+        <div class="content">
+          <Link href="https://lang.pivotstudio.cn/docs/index.html"><div>Documentation</div></Link>
+          <Link href="https://lang.pivotstudio.cn/docs/tutorial/index.html"><div>Tutorial</div></Link>
+          <Link href="https://lang.pivotstudio.cn/docs/references/index.html"><div>API Reference</div></Link>
+        </div>
       </div>
       <div class="line">
         <div class="title">Resources</div>
-        <Link href="https://lang.pivotstudio.cn/docs/blogs/aboutpl.html"><div>Blog</div></Link>
-        <Link href="https://lang.pivotstudio.cn/docs/blogs/lsp_and_salsa.html"><div>lsp and salsa</div></Link>
+        <div class="content">
+          <Link href="https://lang.pivotstudio.cn/docs/blogs/aboutpl.html"><div>Blog</div></Link>
+          <Link href="https://lang.pivotstudio.cn/docs/blogs/lsp_and_salsa.html"><div>lsp and salsa</div></Link>
+        </div>
       </div>
       <div class="line">
         <div class="title">Community</div>
-        <Link href="https://lang.pivotstudio.cn/docs/CONTRIBUTING-CN.html"><div>Contributor's Guide</div></Link>
-        <Link href="https://github.com/Pivot-Studio/pivot-lang/issues"><div>Good First Issue</div></Link>
+        <div class="content">
+          <Link href="https://lang.pivotstudio.cn/docs/CONTRIBUTING-CN.html"><div>Contributor's Guide</div></Link>
+          <Link href="https://github.com/Pivot-Studio/pivot-lang/issues"><div>Good First Issue</div></Link>
+        </div>
       </div>
       <div class="line">
         <div class="title">Contact</div>
-        <Link href="https://github.com/Pivot-Studio/pivot-lang"><div>Github</div></Link>
-        <Link href="https://jq.qq.com/?_wv=1027&k=wcaoBAmg"><div>QQ group</div></Link>
+        <div class="content">
+          <Link href="https://github.com/Pivot-Studio/pivot-lang"><div>Github</div></Link>
+          <Link href="https://jq.qq.com/?_wv=1027&k=wcaoBAmg"><div>QQ group</div></Link>
+        </div>
       </div>
     </div>
   </div>
@@ -40,6 +50,9 @@ import { Link } from '@/components';
   justify-content: center;
   background-color: #253443;
   margin-top: 100px;
+  @media (max-width: 600px) {
+    background-color: transparent;
+  }
 }
 .container {
   max-width: 1200px;
@@ -54,13 +67,35 @@ import { Link } from '@/components';
       margin: 20px;
     }
     .title {
-      text-align: center;
+      text-align: left;
       font-size: 24px;
       font-weight: 700;
       margin-bottom: 30px;
       background: linear-gradient(180deg, rgba(186, 255, 243, 0) 50.04%, rgba(189, 255, 244, 0.44) 100.09%), linear-gradient(64.57deg, #75fff3 7.22%, #599aff 90.07%);
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
+    }
+  }
+  @media (max-width: 600px) {
+    display: block;
+    .line {
+      font-size: 16px;
+      margin: 10px 0;
+      > div {
+        margin: 10px;
+      }
+      .title {
+        font-size: 26px;
+      }
+      .content {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        font-weight: 700;
+        >div {
+          margin-right: 10px;
+          margin-bottom: 30px;
+        }
+      }
     }
   }
 }
